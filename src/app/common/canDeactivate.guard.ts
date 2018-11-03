ada8960aff86816d94efd31df824758501cfdffe
Michael Gira <contact@michaelgira.me>
2018-11-02 20:24:41 -0500

    New angular project

--- src/app/common/canDeactivate.guard.ts
+++
@@ -1,9 +0,0 @@
-export interface CanComponentDeactivate {
-	canDeactivate: () => boolean;
-}
-
-export class CanDeactivateGuard {
-	canDeactivate(component: CanComponentDeactivate): boolean {
-		return component.canDeactivate ? component.canDeactivate() : true;
-	}
-}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-	production: true,
-	backendURL: 'https://api.mymicds.net',
-	recruitMessage: true
+  production: true
 };
